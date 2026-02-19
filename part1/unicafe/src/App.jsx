a55272fdbc6dfdeb2cc3953cdbbf564cba20db64
Tadeo Olivera <tadeooliverabloops@gmail.com>
2026-02-18 23:16:40 -0300

    part1 exercises 1.7-1.8 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,15 +5,17 @@ const Button = ({ handleClick, text }) => <button onClick={() => handleClick(tex
 const Statistic = ({ counter, text }) => <p>{text} {counter}</p>
 
 const App = () => {
-  // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = (good / all) * 100
 
   const handleClick = (text) => {
     switch (text) {
-      case "good": setGood(good + 1); break
-      case "neutral": setNeutral(neutral + 1); break
+      case 'good': setGood(good + 1); break
+      case 'neutral': setNeutral(neutral + 1); break
       default: setBad(bad + 1);
     }
   }
@@ -21,13 +23,16 @@ const App = () => {
   return (
     <>
       <h1>give feedback</h1>
-      <Button handleClick={handleClick} text="good"/>
-      <Button handleClick={handleClick} text="neutral"/>
-      <Button handleClick={handleClick} text="bad"/>
+      <Button handleClick={handleClick} text='good'/>
+      <Button handleClick={handleClick} text='neutral'/>
+      <Button handleClick={handleClick} text='bad'/>
       <h1>statistics</h1>
-      <Statistic counter={good} text="good"/>
-      <Statistic counter={neutral} text="neutral"/>
-      <Statistic counter={bad} text="bad"/>
+      <Statistic counter={good} text='good'/>
+      <Statistic counter={neutral} text='neutral'/>
+      <Statistic counter={bad} text='bad'/>
+      <Statistic counter={all} text='all'/>
+      <Statistic counter={average} text='average'/>
+      <Statistic counter={positive + ' %'} text='positive'/>
     </>
   )
 }
